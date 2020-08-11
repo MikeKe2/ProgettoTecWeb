fae90a75a3658401f8366c96f64cea7541d25c3d
Alessio Pellegrino <seppiabrilla@pop-os.localdomain>
2020-08-11 18:57:11 +0200

    freccie fighe + fighe

--- Creazione_Storia/js/Drawings/frecce.js
+++ Creazione_Storia/js/Drawings/frecce.js
@@ -8,8 +8,31 @@ class freccia{
 		if(board.activegroup === null ||  board.activegroup==this.ngroup){
 			board.context.beginPath();
 			board.context.lineWidth=4;
-			board.context.moveTo((this.from.x + board.const.scene.width/2) * board.scale - board.startX , (this.from.y + board.const.scene.height) * board.scale - board.startY);
-			board.context.lineTo(x?x:(this.to.x + board.const.scene.width/2) * board.scale - board.startX, y?y:this.to.y * board.scale - board.startY);
+			let xF, yF, xT, yT; 
+			if(this.from.x < this.to.x){
+				xF = this.from.x + board.const.scene.width;
+				xT = this.to.x
+			}else if(this.from.x > this.to.x){
+				xF = this.from.x;
+				xT = this.to.x + board.const.scene.width;
+			}else{ 
+				xF = this.from.x + board.const.scene.width / 2;
+				xT = this.to.x + board.const.scene.width / 2;
+			}
+
+			if(this.from.y < this.to.y){
+				yF = this.from.y + board.const.scene.height;
+				yT = this.to.y
+			}else if(this.from.y > this.to.y){
+				yF = this.from.y;
+				yT = this.to.y + board.const.scene.height;
+			}else{ 
+				yF = this.from.y + board.const.scene.height / 2;
+				yT = this.to.y + board.const.scene.height / 2;
+			}
+
+			board.context.moveTo((xF) * board.scale - board.startX , (yF) * board.scale - board.startY);
+			board.context.lineTo(x?x:(xT) * board.scale - board.startX, y?y:yT * board.scale - board.startY);
 			board.context.strokeStyle = storia.categoria=="singolo"?"#000000":groups[this.ngroup];
 			board.context.stroke();
 		}
