7e36d0db4d1620aae028650564993f50a296bf7e
Martiross99 <martiross99@gmail.com>
2020-08-16 14:51:10 +0200

    aggiungere link destinazione qr

--- Player/js/qrCodeScanner.js
+++ Player/js/qrCodeScanner.js
@@ -4,24 +4,22 @@ const video = document.createElement("video");
 const canvasElement = document.getElementById("qr-canvas");
 const canvas = canvasElement.getContext("2d");
 
-const qrResult = document.getElementById("qr-result");
-const outputData = document.getElementById("outputData");
 const btnScanQR = document.getElementById("btn-scan-qr");
 
 let scanning = false;
 
+// TODO: callback di riconoscimento qr, genera link e ci va. 
 qrcode.callback = res => {
   if (res) {
-    outputData.innerText = res;
     scanning = false;
 
     video.srcObject.getTracks().forEach(track => {
       track.stop();
     });
 
-    qrResult.hidden = false;
     canvasElement.hidden = true;
     btnScanQR.hidden = false;
+    window.location.href = "..."; // aggiungere il percorso con parametro get del codice della storia
   }
 };
 
