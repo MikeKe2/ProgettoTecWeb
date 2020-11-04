8f3dc0db408c1c28cb2760ccb8d29cf6ad0e462f
Martiross99 <martiross99@gmail.com>
2020-11-04 17:07:46 +0100

    added route and fixed path

---
+++ Server/public/Player/js/avventura.js
@@ -0,0 +1,32 @@
+var storia;
+var scena_corr = -1;
+
+$.getJSON("/js/test.json", function(data){
+    storiaCallback(data);
+});
+
+function storiaCallback(data){
+    storia = data;
+    console.log(storia);
+    initialize();
+}
+
+function initialize(){
+    $("#titolo").html(storia.nome);
+    $("#btn").click(function(){
+        checkResult(0);
+    })
+}
+
+function checkResult(result){
+    nextScene();
+}
+
+function nextScene(){
+    scena_corr++;
+    $("#testo").html(storia.scene[scena_corr].descrizione);
+    $("#widget").load("../widgets/number.html")
+    if(scena_corr == storia.scene.length-1)
+        $("#btn").hide();
+}
+
