1e1309a28b8bfc3001ee77ea6ed20cc9f293805f
Michele Cucci <michele.cucci@studio.unibo.it>
2021-01-05 14:48:27 +0100

    small fixes

--- Server/public/Manager/js/utility.js
+++ Server/public/Manager/js/utility.js
@@ -1,25 +1,31 @@
 var qrmaker;
-$(document).ready(()=>{
+$(document).ready(() => {
   Stories.init();
   Media.init(Media.types[0]);
   qrmaker = new QRCode($("#qr")[0]);
-  $("#load").on("change",()=>{
+  $("#load").on("change", () => {
     let file = $("#load")[0].files[0];
     console.log(file.name);
-    for(story of Stories.list){
+    for (story of Stories.list) {
       console.log(story);
-      if(story.name == file.name){
-        if(!confirm("Così facendo sovrascriverai il file "+story.name+". Sei sicuro?"))
+      if (story.name == file.name) {
+        if (!confirm("Così facendo sovrascriverai il file " + story.name + ". Sei sicuro?"))
           return;
       }
     }
 
     var reader = new FileReader();
-    reader.onload = (event)=>{
+    reader.onload = (event) => {
       console.log(event.target.result);
       var obj = JSON.parse(event.target.result);
-      $.post("/import", {name: file.name, data:obj},()=>{
-        Stories.list.push({name: file.name, visibility: "private"});
+      $.post("/import", {
+        name: file.name,
+        data: obj
+      }, () => {
+        Stories.list.push({
+          name: file.name,
+          visibility: "private"
+        });
       });
     };
     reader.readAsText($("#load")[0].files[0]);
